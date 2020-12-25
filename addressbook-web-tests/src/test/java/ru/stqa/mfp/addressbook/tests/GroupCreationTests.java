8bac4b313ad2956529b4ca1e65e03fc15f8d72b5
Kirill Konvalinsky <kkanvalinski@gmail.com>
2020-12-25 20:11:36 +0300

    Modified GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.mfp.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,21 +15,29 @@ public class GroupCreationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
     //int before = app.getGroupHelper().getGroupCount();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
-    //int after = app.getGroupHelper().getGroupCount();
-    //Assert.assertEquals(after,before + 1);
+    /*
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after,before + 1);
+    */
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
+    /*
+    {int max = 0;
     for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
+    if (g.getId() > max) {
+    max = g.getId();
     }
-    group.setId(max);
+    }
+    */
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     app.logout();
   }
