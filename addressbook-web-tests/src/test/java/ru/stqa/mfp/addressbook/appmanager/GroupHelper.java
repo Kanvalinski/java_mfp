43c47d2d0d446c0343e95d1a5ab580d62a0b0c52
Kirill Konvalinsky <kkanvalinski@gmail.com>
2020-12-29 21:42:43 +0300

    Modified methods name in GroupTests

--- addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/appmanager/GroupHelper.java
@@ -49,11 +49,25 @@ public class GroupHelper extends HelperBase {
     click(By.name("update"));
   }
 
-  public void createGroup(GroupData group) {
+  public void create(GroupData group) {
    initGroupCreation();
    fillGroupForm(new GroupData("test2", null, null));
    submitGroupCreation();
    returnToGroupPage();
+  }
+
+  public void modify(int index, GroupData group) {
+    selectGroup(index);
+    initGroupModification();
+    fillGroupForm(group);
+    submitGroupModification();
+    returnToGroupPage();
+  }
+
+  public void delete(int index) {
+    selectGroup(index);
+    deleteSelectedGroups();
+    returnToGroupPage();
   }
 
   public boolean isThereAGroup() {
@@ -64,7 +78,7 @@ public class GroupHelper extends HelperBase {
     return wd.findElements(By.name("selected[]")).size();
   }
 
-  public List<GroupData> getGroupList() {
+  public List<GroupData> list() {
     List<GroupData> groups = new ArrayList<GroupData>();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
--- addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/tests/GroupCreationTests.java
@@ -12,26 +12,13 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
-    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.goTo().groupPage();
+    List<GroupData> before = app.group().list();
     //int before = app.getGroupHelper().getGroupCount();
     GroupData group = new GroupData("test2", null, null);
-    app.getGroupHelper().createGroup(group);
-    List<GroupData> after = app.getGroupHelper().getGroupList();
-    /*
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after,before + 1);
-    */
+    app.group().create(group);
+    List<GroupData> after = app.group().list();
     Assert.assertEquals(after.size(), before.size() + 1);
-
-    /*
-    {int max = 0;
-    for (GroupData g : after) {
-    if (g.getId() > max) {
-    max = g.getId();
-    }
-    }
-    */
 
     group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
