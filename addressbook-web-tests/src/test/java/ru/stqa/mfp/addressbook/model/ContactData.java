f5c714ab50dbe6dc0c806a51a19b1d7ac820e059
Kirill Konvalinsky <kkanvalinski@gmail.com>
2021-01-22 19:26:19 +0300

    Created test for Contacts list from DB through ORM Hibernate in HbConnectionTest class

--- addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/mfp/addressbook/model/ContactData.java
@@ -3,54 +3,98 @@ package ru.stqa.mfp.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
   @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id = Integer.MAX_VALUE;
+
   @Expose
+  @Column(name = "firstname")
   private String firstname;
+
   @Expose
+  @Column(name = "middlename")
   private String middlename;
+
   @Expose
+  @Column(name = "lastname")
   private String lastname;
+
   @Expose
+  @Transient
   private String nickname;
+
   @Expose
+  @Transient
   private String title;
+
   @Expose
+  @Transient
   private String company;
+
   @Expose
+  @Transient
   private String address;
+
   @Expose
+  @Column(name = "home")
+  @Type(type = "text")
   private String homePhone;
+
   @Expose
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobilePhone;
+
   @Expose
+  @Column(name = "work")
+  @Type(type = "text")
   private String workPhone;
+
+  @Transient
   private String allPhones;
+  @Transient
   private String allEmails;
+
   @Expose
+  @Transient
   private String email;
   @Expose
+  @Transient
   private String email2;
   @Expose
+  @Transient
   private String email3;
   @Expose
+  @Transient
   private String homepage;
   @Expose
+  @Transient
   private String bday;
   @Expose
+  @Transient
   private String bmonth;
   @Expose
+  @Transient
   private String byear;
   @Expose
+  @Transient
   private String group;
+
   @Expose
-  private File photo;
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String photo;
 
   public int getId() {
     return id;
@@ -137,7 +181,7 @@ public class ContactData {
   }
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   public ContactData withId(int id) {
@@ -235,7 +279,7 @@ public class ContactData {
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
